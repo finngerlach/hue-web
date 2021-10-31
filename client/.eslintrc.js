f2307db9b170ed256e3eb8ef781011629789782f
Finn Gerlach <72390121+finngerlach@users.noreply.github.com>
2021-10-31 22:17:40 +0100

    add eslint configuration

---
+++ client/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 'error', 'unix' ],
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'never' ]
+  }
+}
