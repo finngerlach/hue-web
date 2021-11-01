aa1d790d373b3a855ebd12fcddd1b3e7829c7ed6
Finn Gerlach <72390121+finngerlach@users.noreply.github.com>
2021-11-01 22:45:50 +0100

    feat(lint): add eslint vue plugin

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,9 +2,6 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
