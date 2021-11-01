0d93a29825015b58c939c5015dafeaae4624aa75
Finn Gerlach <72390121+finngerlach@users.noreply.github.com>
2021-11-01 22:12:35 +0100

    feat(api): add bridge discovery
    Add express graphql endpoint.
    Implement fast and slow bridge discovery GQL types and resolvers.

--- api/src/index.js
+++ api/src/index.js
@@ -4,9 +4,22 @@ const app = express()
 const config = require('config')
 const port = config.get('port')
 
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./graphql/schemas')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: {
+      headerEditorEnabled: true
+    }
+  })
+)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ api/src/services/bridge.service.js
@@ -0,0 +1,29 @@
+const hue = require('node-hue-api')
+
+module.exports = {
+  async discoverBridgesFast() {
+    const results = await hue.discovery.nupnpSearch()
+
+    return results.map(entry => {
+      if (entry.config) return {
+        name: entry.config.name,
+        ip: entry.ipaddress
+      }
+      else return {
+        message: entry.message,
+        description: entry.description
+      }
+    })
+  },
+
+  async discoverBridgesSlow() {
+    const results = await hue.discovery.upnpSearch()
+
+    return results.map(entry => {
+      return {
+        name: entry.name,
+        ip: entry.ipaddress
+      }
+    })
+  }
+}
